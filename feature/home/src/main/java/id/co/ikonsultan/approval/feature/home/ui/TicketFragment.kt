0d3a75b6bd505700c653fb310c735ff8038a3971
Dedy Wijaya <Boys.mtv@gmail.com>
2026-02-24 13:17:28 +0700

    chore: add home & ticket

---
+++ feature/home/src/main/java/id/co/ikonsultan/approval/feature/home/ui/TicketFragment.kt
@@ -0,0 +1,14 @@
+/*
+ * Project: Shopme App
+ * Author: Boys.mtv@gmail.com
+ * File: TicketFragment.kt
+ *
+ * Last modified by Dedy Wijaya on 24/02/26 13.03
+ */
+
+package id.co.ikonsultan.approval.feature.home.ui
+
+import androidx.fragment.app.Fragment
+import id.co.ikonsultan.approval.feature.home.R
+
+class TicketFragment : Fragment(R.layout.fragment_ticket)
